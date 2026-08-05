685ed0c9060bd122245663a259c55d066f8cb318
Sohrab <hi.mdsohrab@gmail.com>
2026-08-06 01:06:59 +0600

    Added Chat_Model

---
+++ Chat_Model/chatgroq.py
@@ -0,0 +1,12 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGroq(
+    model = "llama-3.3-70b-versatile",
+)
+
+result = model.invoke("Write me a poem on AI")
+
+print(result.content)
